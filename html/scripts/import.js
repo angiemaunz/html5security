faf63234c3e3197195188cd5bd90e80c889a1fd7
mario.heiderich <mario.heiderich@d0ae2a4b-d8d3-47d4-0f2e-ffd188ab4906>
2010-05-01 14:05:03 +0000

    * implemented the first translation logic

--- html/scripts/import.js
+++ html/scripts/import.js
@@ -4,6 +4,8 @@
 (function(){
     window.onload = function() {
         (function() {
+            
+            // enumerate categories and build initial lists
             for(var category in categories) {
                 $('#categories').append('<li id="'+category+'"><h3>'+categories[category]+'</h3></li>');
                 $('#sidebar').append('<li><a href="#'+category+'">'+categories[category]+'</a></li>');
@@ -13,24 +15,40 @@
         })();
         (function() {
             for (var item in items) {
+                
+                // replace the payload templates
                 for (var payload in payloads) {
                     var regex = new RegExp('%' + payload + '%');
                     items[item].data = items[item].data.replace(regex, payloads[payload]);
                 }
+                // sanitize the input
                 items[item].data = items[item].data.replace(/&/gm, '&amp;');
                 items[item].data = items[item].data.replace(/</gm, '&lt;');
                 items[item].data = items[item].data.replace(/>/gm, '&gt;');                
                 
+                // build markup container for the content
                 var li = $('#categories li#'+items[item].category+' h3');
                 var container = $($('#item_template').html());
                 
                 for(var c in items[item]) {
-                    if(typeof items[item][c] === 'string') {
+                    
+                    // determine navigator language or set default
+                    var lang = navigator.language ? navigator.language : 'en';
+                    if(typeof items[item][c][lang] === 'undefined' || !items[item][c][lang]) {
+                        lang = 'en';
+                    }
+                    // check if translated items exist
+                    if(typeof items[item][c][lang] === 'string') {
+                        items[item][c][lang] = items[item][c][lang].replace(/</gm, '&lt;');
+                        items[item][c][lang] = items[item][c][lang].replace(/>/gm, '&gt;');                        
+                        container.find('.'+c).html(items[item][c][lang]);        
+                    } else if(typeof items[item][c] === 'string') {
                         items[item][c] = items[item][c].replace(/</gm, '&lt;');
                         items[item][c] = items[item][c].replace(/>/gm, '&gt;');                        
-                        container.find('.'+c).html(items[item][c]);        
+                        container.find('.'+c).html(items[item][c]);   
                     }
                 }
+                // fill browser list
                 if(items[item].browsers) {
                     for(var browser in items[item].browsers) {
                         container.find('.browsers').append('<ul class="'+browser+'" />');
@@ -43,12 +61,14 @@
                     }
                     container.find('.browsers').append('<span class="clear"></span>');
                 }   
+                // fill tag list
                 if(items[item].tags) {
                     for(var tag in items[item].tags) {
                         container.find('.tags').append('<li>'+items[item].tags[tag]+'</li>');
                     } 
 					container.find('.tags').append('<span class="clear"></span>');
                 }   
+                // fill ticket list
                 if(items[item].tickets) {
                     for(var ticket in items[item].tickets) {
                         container.find('.tickets').append(
