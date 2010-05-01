4bd55d98242adf46d25afb67afa213968f786b5f
mario.heiderich <mario.heiderich@d0ae2a4b-d8d3-47d4-0f2e-ffd188ab4906>
2010-05-01 10:03:57 +0000

    * added three more vectors

--- html/scripts/import.js
+++ html/scripts/import.js
@@ -16,6 +16,7 @@
                 for (var payload in payloads) {
                     var regex = new RegExp('%' + payload + '%');
                     items[item].data = items[item].data.replace(regex, payloads[payload]);
+                    items[item].data = items[item].data.replace(/&/gm, '&amp;');
                     items[item].data = items[item].data.replace(/</gm, '&lt;');
                     items[item].data = items[item].data.replace(/>/gm, '&gt;');
                 }
@@ -24,6 +25,7 @@
                 
                 for(var c in items[item]) {
                     if(typeof items[item][c] === 'string') {
+                        items[item][c] = items[item].data.replace(/&/gm, '&amp;');
                         items[item][c] = items[item][c].replace(/</gm, '&lt;');
                         items[item][c] = items[item][c].replace(/>/gm, '&gt;');                        
                         container.find('.'+c).html(items[item][c]);        
